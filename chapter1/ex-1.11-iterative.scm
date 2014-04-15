7c959c28a0baa2008c79a579c7f02313203f7b00
Daniel-Xu <xwd.forever@gmail.com>
2014-04-15 18:01:19 -0400

    one is iterative process  one is recursive process
    recursive process is more intuitive
    iterative process needs to know these variable needed to be saved

---
+++ chapter1/ex-1.11-iterative.scm
@@ -0,0 +1,14 @@
+;we need to know what we should save 
+(define (f n) 
+  (if (<  n 3)
+      n
+      (f-helper 2 1 0 3 n))) 
+
+(define (f-helper minus1 minus2 minus3 counter n)
+  (if (> counter n)
+      (+ minus1 (* 2 minus2) (* 3 minus3))
+      (f-helper (+ minus1 (* 2 minus2) (* 3 minus3))
+                minus1
+                minus2
+                (+ counter 1) 
+                n)))
