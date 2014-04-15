66d9947c2e34e7f79faa19e93b3c45f6f0ae4e1d
Daniel-Xu <xwd.forever@gmail.com>
2014-04-15 18:38:32 -0400

    if n > 3
    n = 3 run 3a + 2b + c once  (3-2)
    n = 4 run 3a + 2b + c twice (4-2)

--- chapter1/ex-1.11-iterative.scm
+++ chapter1/ex-1.11-iterative.scm
@@ -1,14 +1,26 @@
 ;we need to know what we should save 
-(define (f n) 
-  (if (<  n 3)
-      n
-      (f-helper 2 1 0 3 n))) 
 
-(define (f-helper minus1 minus2 minus3 counter n)
-  (if (>= counter n)
-      (+ minus1 (* 2 minus2) (* 3 minus3))
-      (f-helper (+ minus1 (* 2 minus2) (* 3 minus3))
-                minus1
-                minus2
-                (+ counter 1) 
-                n)))
+;(define (f n) 
+  ;(if (<  n 3)
+      ;n
+      ;(f-helper 2 1 0 3 n))) 
+
+;(define (f-helper minus1 minus2 minus3 counter n)
+  ;(if (>= counter n)
+      ;(+ minus1 (* 2 minus2) (* 3 minus3))
+      ;(f-helper (+ minus1 (* 2 minus2) (* 3 minus3))
+                ;minus1
+                ;minus2
+                ;(+ counter 1) 
+                ;n)))
+
+(define (f n)
+  ;3a  2b c
+  (define (iter a b c n)
+    (if (< n 3)
+        c 
+        (iter b c (+ (* 3 a) (* 2 b) c) (- n 1))
+        ))
+  (if (< n 3)
+    n)
+    (iter 0 1 2 n))
