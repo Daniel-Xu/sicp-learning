eeea729d364c5740c4c769430d7412c26eeac62c
Daniel-Xu <xwd.forever@gmail.com>
2014-04-15 18:12:30 -0400

    bug for the boundary

--- chapter1/ex-1.11-iterative.scm
+++ chapter1/ex-1.11-iterative.scm
@@ -5,7 +5,7 @@
       (f-helper 2 1 0 3 n))) 
 
 (define (f-helper minus1 minus2 minus3 counter n)
-  (if (> counter n)
+  (if (>= counter n)
       (+ minus1 (* 2 minus2) (* 3 minus3))
       (f-helper (+ minus1 (* 2 minus2) (* 3 minus3))
                 minus1
