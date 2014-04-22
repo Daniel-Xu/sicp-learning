342f2bca57a22d6ce4e2d0244abf2799a1f64aaf
Daniel-Xu <xwd.forever@gmail.com>
2014-04-22 09:44:55 -0400

    iterative version

--- chapter1/1.3/ex-1.43-repeat.scm
+++ chapter1/1.3/ex-1.43-repeat.scm
@@ -1,8 +1,29 @@
 
-(define (repeat f n)
-  (if (< n 1)
-      (lambda (x) x)
-      (lambda (x)
-        (f ( (repeat f (- n 1)) x)))))
+;(define (repeat f n)
+  ;(if (< n 1)
+      ;(lambda (x) x)
+      ;(lambda (x)
+        ;(f ( (repeat f (- n 1)) x)))))
+
+
+
+
+;iterative procedure
 
 (define (square x) (* x x))
+
+; return procedure
+(define (composite f g)
+  (lambda (x) (f (g x))))
+
+
+; result is a procedure
+(define (repeat f n)
+  (define (iter n result) 
+    (if (< n 1)
+        result
+        (iter (- n 1) (composite f result))))
+  (iter n (lambda (x) x)))
+
+
+
